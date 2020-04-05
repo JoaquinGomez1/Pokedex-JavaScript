395567dcb95e0ec6395cfd9122325e6876f2d6dd
JoaquinGomez1 <joaquingomez3098@gmail.com>
2020-04-05 20:31:33 -0300

    Added info screen for each pokemon

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 class myPokemon{
-	constructor (name, id, img, type){
+	constructor (name, id, img, type,abilities, stats, gamesFound){
 	this.name = name
 	this.id   = id
 	this.img  = img
 	this.type = type
+	this.abilities = abilities
+	this.stats = stats
+	this.gamesFound = gamesFound
 	}
 }
 
 var pokemonList = [];
+let input = document.querySelector('.searcher')
+let pokemonGenerationAmount = 386
+
 
 // Make a request to the API, convert it into json and add a myPokemon object to the list.
-for(let i = 1; i <= 386; i++){
+for(let i = 1; i <= pokemonGenerationAmount; i++){
 	pokemonList.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
 	.then((res) => {return res.json()}).then((data)=>{
-		console.log(data)
-		return new myPokemon(data.name,data.id, data.sprites['front_default'],data.types );
+		return new myPokemon(data.name,data.id, data.sprites['front_default']
+			,data.types, data.abilities[0].ability.name, data.stats, data.game_indices );
 	}))
 }
 
@@ -25,11 +31,12 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  	newRow.classList.add('row');
  	pkmInfo.appendChild(newRow)
 
- 	for(let i = 1; i <= 386; i++){ // Creating each pokemon card.
- 		let pkmName = result[i - 1].name;
- 		let pkmId   = result[i - 1].id;
- 		let pkmImg  = result[i - 1].img;
- 		let pkmType = getPrimaryType(result[i - 1].type); // Pokemon's main type.
+ 	for(let i = 1; i <= pokemonGenerationAmount; i++){ // Creating each pokemon card.
+ 		let pokemon = result[i - 1]
+ 		let pkmName = pokemon.name;
+ 		let pkmId   = pokemon.id;
+ 		let pkmImg  = pokemon.img;
+ 		let pkmType = getPrimaryType(pokemon.type); // Pokemon's main type.
 		let newDiv  = document.createElement('div');
  		let imgElem = document.createElement('img');
  		let tipeDiv = document.createElement('div');
@@ -42,10 +49,10 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
 
  		newDiv.classList.add('col-md-2');  
  		newDiv.classList.add('pokemon-card');
- 		newDiv.classList.add(`pokemon-${i}`); // Each pokemon has a class that identifies them.
+ 		newDiv.setAttribute('id', `pokemon-${pkmId}`) // Each pokemon has a class that identifies them.
+ 		newDiv.setAttribute('onclick', `displayInfoScreen(${pkmId})`)
  		newDiv.innerHTML = `N. ${pkmId}  ${pkmName}`;
  		newDiv.style.backgroundColor = getBackgroundColor(pkmType);
- 		//newDiv.onclick = displayInfoScreen(result[i - 1]);
 
  		newDiv.appendChild(tipeDiv);
  		newDiv.appendChild(imgElem);
@@ -83,17 +90,21 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  }
 
 
-function displayInfoScreen(pkmId){
-	let infoScreen = querySelector('.pkmInfoScreen');
-}
+input.addEventListener('keyup', (event) =>{
+	if(event.keyCode === 13){ // Number 13 is the code for the enter key
+		event.preventDefault()
+		document.getElementById("searchBtn").click();
+	} 
+})
+
 
 function searchPkm(){
 	Promise.all(pokemonList).then((result) =>{
-		let input = document.querySelector('.searcher').value;
-		input = input.toLowerCase();
+		input = document.querySelector('.searcher')
+		input = input.value.toLowerCase();
 		for(let i = 1; i < pokemonList.length; i++){
 			if(result[i].name == input || result[i].id == input){
-				let goTo = document.querySelector(`.pokemon-${result[i].id}`);
+				let goTo = document.getElementById(`pokemon-${result[i].id}`);
  				goTo.scrollIntoView({block: "center"});
 			}
 		}
@@ -113,5 +124,99 @@ function getPrimaryType(types){
 		
 	}else{
 		return types[0].type.name
+	}
+}
+
+function displayInfoScreen(pokemonId){
+
+	let infoScr     = document.querySelector('.infoScr');
+	let closeBtn    = document.querySelector('.closeInfoScr');
+	let infoContent = document.querySelector('.scrPkmInfo');
+
+	let controlList = []
+	let controlClasses = ['pkmImgInfo', 'pkmNameInfo',
+	'pkmTypesInfo', 'pkmIdInfo','pkmStatsInfo','pkmAbilitiesInfo']
+
+	for(let i = 0; i < 6 ; i++){
+		if(i == 0){
+			let myVar = document.createElement('img')
+			myVar.classList.add(controlClasses[i])
+			controlList.push(myVar)
+		}else{
+			let myVar = document.createElement('div')
+			myVar.classList.add(controlClasses[i])
+			controlList.push(myVar)
+		}
+	}
+	// let imgDiv = document.createElement('img');
+	// let nameDiv = document.createElement('div');
+	// let typesDiv = document.createElement('div');
+	// let idDiv = document.createElement('div');
+	// let statsDiv = document.createElement('div');
+	// let abilityDiv = document.createElement('div');
+
+	// imgDiv.classList.add('pkmImgInfo')
+	// nameDiv.classList.add('pkmNameInfo')
+	// typesDiv.classList.add('pkmTypesInfo')
+	// idDiv.classList.add('pkmIdInfo')
+	// statsDiv.classList.add('pkmStatsInfo')
+
+
+
+	infoScr.style.display = 'block';
+
+			// Closing infoScreen:
+	window.onclick = (event)=>{
+		if(event.target == infoScr){
+			infoScr.style.display = 'none';
+			closeScreen(controlClasses);
+		}
+	}
+	closeBtn.onclick = ()=>{
+		infoScr.style.display = 'none';
+		closeScreen(controlClasses);
+	}
+
+	Promise.all(pokemonList).then((result)=>{
+		let thisPokemon = result[pokemonId - 1]
+		console.log(thisPokemon);
+		let pokemonContent = ['img','name','type.type.name', 'id', 'stats','abilities']
+
+		for(let i = 0; i < 6; i++){
+			if(i == 0){
+				controlList[i].src = thisPokemon.img;
+				infoContent.appendChild(controlList[i])
+			}else{
+				controlList[i].innerHTML = thisPokemon[pokemonContent[i]]
+				infoContent.appendChild(controlList[i])
+			}
+		}
+
+		
+
+		// imgDiv.src = thisPokemon.img;
+		// nameDiv.innerHTML = thisPokemon.name;
+		// typesDiv.innerHTML = thisPokemon.types;
+		// idDiv.innerHTML = thisPokemon.id;
+		// statsDiv.innerHTML = thisPokemon.stats;
+
+		// infoContent.appendChild(imgDiv);
+		// infoContent.appendChild(nameDiv);
+		// infoContent.appendChild(typesDiv);
+		// infoContent.appendChild(idDiv);
+		// infoContent.appendChild(statsDiv);
+
+	})
+}
+
+function closeScreen(controlClasses){
+	// let img = document.querySelector('.pkmImgInfo')
+	 let infoContent = document.querySelector('.scrPkmInfo');
+
+	// infoContent.removeChild(img);
+
+	for(let i = 0; i < 6; i++){
+		let element = document.querySelector(`.${controlClasses[i]}`)
+		infoContent.removeChild(element);
 	}
 }
