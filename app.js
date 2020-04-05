0f1230c2c7f5423005ff4e325d64e7138dc4814d
JoaquinGomez1 <joaquingomez3098@gmail.com>
2020-04-05 01:39:54 -0300

    Added search button functionality

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ class myPokemon{
 
 var pokemonList = [];
 
-// Make a request to the APO, convert it into json and add a myPokemon object to the list.
-for(let i = 1; i <= 151; i++){
+// Make a request to the API, convert it into json and add a myPokemon object to the list.
+for(let i = 1; i <= 386; i++){
 	pokemonList.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
 	.then((res) => {return res.json()}).then((data)=>{
+		console.log(data)
 		return new myPokemon(data.name,data.id, data.sprites['front_default'],data.types );
 	}))
 }
@@ -24,11 +25,11 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  	newRow.classList.add('row');
  	pkmInfo.appendChild(newRow)
 
- 	for(let i = 1; i <= 151; i++){ // Empezamos a crear los contenedores de cada pokemón.
+ 	for(let i = 1; i <= 386; i++){ // Creating each pokemon card.
  		let pkmName = result[i - 1].name;
  		let pkmId   = result[i - 1].id;
  		let pkmImg  = result[i - 1].img;
- 		let pkmType = getPrimaryType(result[i - 1].type); // Este es el tipo principal del pokemon.
+ 		let pkmType = getPrimaryType(result[i - 1].type); // Pokemon's main type.
 		let newDiv  = document.createElement('div');
  		let imgElem = document.createElement('img');
  		let tipeDiv = document.createElement('div');
@@ -41,15 +42,18 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
 
  		newDiv.classList.add('col-md-2');  
  		newDiv.classList.add('pokemon-card');
- 		newDiv.classList.add(`pokemon-${i}`); // Each pokemon has an class that identifies them.
- 		newDiv.innerHTML = `N°: ${pkmId}  ${pkmName}`;
+ 		newDiv.classList.add(`pokemon-${i}`); // Each pokemon has a class that identifies them.
+ 		newDiv.innerHTML = `N. ${pkmId}  ${pkmName}`;
  		newDiv.style.backgroundColor = getBackgroundColor(pkmType);
+ 		//newDiv.onclick = displayInfoScreen(result[i - 1]);
 
  		newDiv.appendChild(tipeDiv);
  		newDiv.appendChild(imgElem);
  		newRow.appendChild(newDiv);
  	}
  })
+
+
 
 
  function getBackgroundColor(pokmType){
@@ -65,30 +69,48 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  		fighting: '#c28a5f', 
  		rock: '#9e7642',
  		ground: '#7d5725',
- 		normal: '#546170',
- 		fairy: '#cf257d',
+ 		normal: '#a8a090',
+ 		fairy: '#d492d4',
  		ice: '#96d0d9',
  		dragon: '#a81828',
  		steel: '#809296',
- 		ghost: '#957699'
+ 		ghost: '#6060b0',
+ 		dark: '#4d5054'
 
  	}
- 	return objColors[pokmType];  // pokmType has to be in square brackets because it's a string
+ 	// pokmType has to be in square brackets because the parameter passed to the function it's a string
+ 	return objColors[pokmType];  
  }
 
 
-function searchPkm(){
- 	let input = document.querySelector('.searcher').value;
- 	let goTo = document.querySelector(`.pokemon-${input}`);
- 	console.log(input);
- 	goTo.scrollIntoView();
+function displayInfoScreen(pkmId){
+	let infoScreen = querySelector('.pkmInfoScreen');
 }
 
-/* The API returns a list wich contains each pokemon type.
-if a pokemon has two different types it's main one is the one in the first index*/
+function searchPkm(){
+	Promise.all(pokemonList).then((result) =>{
+		let input = document.querySelector('.searcher').value;
+		input = input.toLowerCase();
+		for(let i = 1; i < pokemonList.length; i++){
+			if(result[i].name == input || result[i].id == input){
+				let goTo = document.querySelector(`.pokemon-${result[i].id}`);
+ 				goTo.scrollIntoView({block: "center"});
+			}
+		}
+	})
+}
+
 function getPrimaryType(types){ 
 	if(types.length >= 2){
-		return types[1].type.name
+		if(types[0].type.name == 'normal'){
+			return types[1].type.name;
+
+		}else if(types[1].type.name == 'normal'){
+			return types[0].type.name;
+
+		}else{
+			return types[1].type.name;}
+		
 	}else{
 		return types[0].type.name
 	}
