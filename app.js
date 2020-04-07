5696aeb192d2b4c330eb3dc266e18f22a0e630c9
JoaquinGomez1 <joaquingomez3098@gmail.com>
2020-04-07 19:18:08 -0300

    Cleaned the code a little bit more

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 class myPokemon{
-	constructor (name, id, img, type,abilities, stats, gamesFound){
+	constructor (name, id, img, type,abilities, stats){
 	this.name = name
 	this.id   = id
 	this.img  = img
 	this.type = type
 	this.abilities = abilities
 	this.stats = stats
-	this.gamesFound = gamesFound
 	}
 }
 
 var pokemonList = [];
 let input = document.querySelector('.searcher');
-let pokemonGenerationAmount = 151;
+let pokemonGenerationAmount = 150;
 
 
 // Make a request to the API, convert it into json and add a myPokemon object to the list.
-for(let i = 1; i <= pokemonGenerationAmount; i++){
-	pokemonList.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+for(let i = 0; i <= pokemonGenerationAmount; i++){
+	pokemonList.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`) // adding +1 to i because pokemon's Id's start at one
 	.then((res) => {return res.json()}).then((data)=>{
 		return new myPokemon(data.name,data.id, data.sprites['front_default']
-			,data.types, data.abilities[0].ability.name, data.stats, data.game_indices );
+			,data.types, data.abilities[0].ability.name, data.stats);
 	}))
 }
 
@@ -31,8 +30,8 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  	newRow.classList.add('row');
  	pkmInfo.appendChild(newRow)
 
- 	for(let i = 1; i <= pokemonGenerationAmount; i++){ // Creating each pokemon card.
- 		let pokemon = result[i - 1]
+ 	for(let i = 0; i <= pokemonGenerationAmount; i++){ // Creating each pokemon card.
+ 		let pokemon = result[i]	// 
  		let pkmName = pokemon.name;
  		let pkmId   = pokemon.id;
  		let pkmImg  = pokemon.img;
@@ -50,7 +49,7 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  		newDiv.classList.add('col-md-2');  
  		newDiv.classList.add('pokemon-card');
  		newDiv.setAttribute('id', `pokemon-${pkmId}`) // Each pokemon has a class that identifies them.
- 		newDiv.setAttribute('onclick', `displayInfoScreen(${pkmId})`)
+ 		newDiv.setAttribute('onclick', `displayModal(${pkmId})`)
  		newDiv.innerHTML = `N. ${pkmId}  ${pkmName}`;
  		newDiv.style.backgroundColor = getBackgroundColor(pkmType);
 
@@ -60,8 +59,121 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
  	}
  })
 
- function getBackgroundColor(pokmType){
- 	const objColors = {
+ 	// If the users presses the enter key trigger 'searchBtn()'
+input.addEventListener('keyup', (event) =>{
+	if(event.keyCode === 13){
+		event.preventDefault()
+		document.getElementById("searchBtn").click();
+	} 
+})
+
+
+function searchPkm(){
+	Promise.all(pokemonList).then((result) =>{
+		input = document.querySelector('.searcher')
+		input = input.value.toLowerCase();
+		for(let i = 0; i < pokemonList.length; i++){
+			if(result[i].name == input || result[i].id == input){
+				let goTo = document.getElementById(`pokemon-${result[i].id}`);
+ 				goTo.scrollIntoView({block: "center"});
+ 				displayModal(result[i].id);
+
+			}
+		}
+	})
+}
+
+
+function displayModal(pokemonId){
+	let infoScr     = document.querySelector('.infoScr');
+	let closeBtn    = document.querySelector('.closeInfoScr');
+	let infoContent = document.querySelector('.scrPkmInfo');
+	let documentsList = []
+	let documentsClasses = ['pkmImgInfo', 'pkmNameInfo',
+	'pkmTypesInfo', 'pkmIdInfo','pkmStatsInfo','pkmAbilitiesInfo']
+	let classesAmount = documentsClasses.length;
+
+	for(let i = 0; i < classesAmount; i++){
+		if(documentsClasses[i] == 'pkmImgInfo'){
+			let thisDocument = document.createElement('img')
+			thisDocument.classList.add(documentsClasses[i])
+			documentsList.push(thisDocument)
+		}else{
+			let thisDocument = document.createElement('div')
+			thisDocument.classList.add(documentsClasses[i])
+			documentsList.push(thisDocument)
+		}
+	}
+
+	infoScr.style.display = 'block';
+	createModalContent(pokemonId, documentsList);
+
+			// Closing infoScreen:
+	window.addEventListener('keyup', (event)=>{
+		if(event.key == 'Escape' && infoScr.style.display != 'none'){
+			infoScr.style.display = 'none';
+			closeModal(documentsClasses);
+		}
+	})
+
+	window.onclick = (event)=>{
+		if(event.target == infoScr){
+			infoScr.style.display = 'none';
+			closeModal(documentsClasses);
+		}
+	}
+
+	closeBtn.onclick = ()=>{
+		infoScr.style.display = 'none';
+		closeModal(documentsClasses);
+	}
+}
+
+function createModalContent(pokemonId, documentsList){ // TODO: improve if/else logic.
+	Promise.all(pokemonList).then((result)=>{  // Creating the modal's content
+		let thisPokemon = result[pokemonId - 1]
+		let pokemonAttributes = ['img','name', 'types', 'id', 'stats','abilities']
+		let pokemonTypesList = getBothTypes(thisPokemon);
+		let pokemonStatsList = getPokemonStats(thisPokemon);
+		let infoContent = document.querySelector('.scrPkmInfo');
+
+		for(let i = 0; i < pokemonAttributes.length; i++){
+			if(pokemonAttributes[i] == 'img'){
+				documentsList[i].src = thisPokemon.img;
+				infoContent.appendChild(documentsList[i]);
+			}else if(pokemonAttributes[i] == 'types'){
+				documentsList[i].innerHTML = pokemonTypesList;
+				infoContent.appendChild(documentsList[i]);
+			}else if(pokemonAttributes[i] == 'stats'){
+				documentsList[i].innerHTML = pokemonStatsList.join('<br>');
+				infoContent.appendChild(documentsList[i]);
+			}else if(pokemonAttributes[i] == 'id'){
+				documentsList[i].innerHTML = 'Id Number: '+ thisPokemon[pokemonAttributes[i]];
+				infoContent.appendChild(documentsList[i]);
+			}
+			else if(pokemonAttributes[i] == 'abilities'){
+				documentsList[i].innerHTML = 'Ability: ' + thisPokemon[pokemonAttributes[i]];
+				infoContent.appendChild(documentsList[i]);
+			}
+			else{
+				documentsList[i].innerHTML = thisPokemon[pokemonAttributes[i]];
+				infoContent.appendChild(documentsList[i]);
+			}
+		}
+	})
+}
+
+function closeModal(documentsClasses){
+	let classesAmount = 6;
+	let infoContent = document.querySelector('.scrPkmInfo');
+	for(let i = 0; i < classesAmount; i++){
+		let element = document.querySelector(`.${documentsClasses[i]}`);
+		infoContent.removeChild(element);
+	}
+}
+
+function getBackgroundColor(pokmType){
+ 	const COLORS_BY_TYPE = {
  		fire: '#d95e3f',
  		water: '#5aaedb',
  		grass: '#48bd5f',
@@ -83,32 +195,25 @@ Promise.all(pokemonList)  //Wait for every result to load before showing them to
 
  	}
  	// pokmType has to be in square brackets because the parameter passed to the function it's a string
- 	return objColors[pokmType];  
- }
-
-
-input.addEventListener('keyup', (event) =>{
-	if(event.keyCode === 13){ // Number 13 is the code for the enter key
-		event.preventDefault()
-		document.getElementById("searchBtn").click();
-	} 
-})
-
-
-function searchPkm(){
-	Promise.all(pokemonList).then((result) =>{
-		input = document.querySelector('.searcher')
-		input = input.value.toLowerCase();
-		for(let i = 1; i < pokemonList.length; i++){
-			if(result[i].name == input || result[i].id == input){
-				let goTo = document.getElementById(`pokemon-${result[i].id}`);
- 				goTo.scrollIntoView({block: "center"});
- 				displayInfoScreen(result[i].id);
-
-			}
-		}
-	})
+ 	return COLORS_BY_TYPE[pokmType];  
 }
+
+function getBothTypes(pokemon){
+			if(pokemon.type.length == 2){
+					return `${pokemon.type[0].type.name} - ${pokemon.type[1].type.name}`
+			}else{
+				return pokemon.type[0].type.name;
+			}
+}
+
+function getPokemonStats(pokemon){
+	let list = []
+	for(let i = 0; i < pokemon.stats.length; i++){
+		list.push(`${pokemon.stats[i].stat.name}: ${pokemon.stats[i].base_stat}`)
+	}
+	return list
+}
+
 
 function getPrimaryType(types){ 
 	if(types.length >= 2){
@@ -124,111 +229,4 @@ function getPrimaryType(types){
 	}else{
 		return types[0].type.name
 	}
-}
-
-function getBothTypes(pokemon){
-			if(pokemon.type.length == 2){
-					return [pokemon.type[0].type.name, pokemon.type[1].type.name]
-			}else{
-				return pokemon.type[0].type.name;
-			}
-}
-
-function getPokemonStats(pokemon){
-	let list = []
-	for(let i = 0; i < pokemon.stats.length; i++){
-		list.push(`${pokemon.stats[i].stat.name}: ${pokemon.stats[i].base_stat}`)
-	}
-	return list
-}
-
-function displayInfoScreen(pokemonId){
-	let infoScr     = document.querySelector('.infoScr');
-	let closeBtn    = document.querySelector('.closeInfoScr');
-	let infoContent = document.querySelector('.scrPkmInfo');
-	let controlList = []
-	let controlClasses = ['pkmImgInfo', 'pkmNameInfo',
-	'pkmTypesInfo', 'pkmIdInfo','pkmStatsInfo','pkmAbilitiesInfo']
-
-	for(let i = 0; i < 6 ; i++){
-		if(i == 0){
-			
-			let myVar = document.createElement('img')
-			myVar.classList.add(controlClasses[i])
-			controlList.push(myVar)
-		}else{
-			
-			let myVar = document.createElement('div')
-			myVar.classList.add(controlClasses[i])
-			controlList.push(myVar)
-		}
-	}
-
-	infoScr.style.display = 'block';
-
-			// Closing infoScreen:
-
-	window.addEventListener('keyup', (event)=>{
-		if(event.key == 'Escape' && infoScr.style.display != 'none'){
-			infoScr.style.display = 'none';
-			closeScreen(controlClasses);
-		}
-	})
-
-	window.onclick = (event)=>{
-		if(event.target == infoScr){
-			infoScr.style.display = 'none';
-			closeScreen(controlClasses);
-		}
-	}
-	closeBtn.onclick = ()=>{
-		infoScr.style.display = 'none';
-		closeScreen(controlClasses);
-	}
-
-	
-	createInfoScrContent(pokemonId, controlList);
-
-}
-
-function closeScreen(controlClasses){
-	let infoContent = document.querySelector('.scrPkmInfo');
-	for(let i = 0; i < 6; i++){
-		let element = document.querySelector(`.${controlClasses[i]}`);
-		infoContent.removeChild(element);
-	}
-}
-
-function createInfoScrContent(pokemonId, controlList){ // TODO: improve if/else logic.
-	Promise.all(pokemonList).then((result)=>{  // Creating the information screen's content
-		let thisPokemon = result[pokemonId - 1]
-		let pokemonContent = ['img','name', 'types', 'id', 'stats','abilities']
-		let pokemonTypesList = getBothTypes(thisPokemon);
-		let pokemonStatsList = getPokemonStats(thisPokemon);
-		let infoContent = document.querySelector('.scrPkmInfo');
-
-		for(let i = 0; i < pokemonContent.length; i++){
-			if(i == 0){
-				controlList[i].src = thisPokemon.img;
-				infoContent.appendChild(controlList[i]);
-			}else if(i == 2){
-				controlList[i].innerHTML = pokemonTypesList;
-				infoContent.appendChild(controlList[i]);
-			}else if(pokemonContent[i] == 'stats'){
-				controlList[i].innerHTML = pokemonStatsList;
-				infoContent.appendChild(controlList[i]);
-			}else if(pokemonContent[i] == 'id'){
-				controlList[i].innerHTML = 'Id Number: '+ thisPokemon[pokemonContent[i]];
-				infoContent.appendChild(controlList[i]);
-			}
-			else if(pokemonContent[i] == 'abilities'){
-				controlList[i].innerHTML = 'Ability: ' + thisPokemon[pokemonContent[i]];
-				infoContent.appendChild(controlList[i]);
-			}
-			else{
-				controlList[i].innerHTML = thisPokemon[pokemonContent[i]];
-				infoContent.appendChild(controlList[i]);
-			}
-		}
-	})
 }
